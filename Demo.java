7da120c3757dee7f6011abc0da86a84e46a31aae
Hemant Borase <hborase004@gmail.com>
2026-03-27 20:51:16 +0530

    msg

---
+++ Demo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String[] args)
+	{
+	   System.out.println("Enter the Size of Array:");
+	   Scanner sc=new Scanner(System.in);
+	   int n=sc.nextInt();
+	   
+	   int[] arr=new int[n];
+	   System.out.println("Enter the Array Elements:");
+	   int sum=1;
+	   for(int i=0;i<=n-1;i++)
+	   {
+	      arr[i]=sc.nextInt();
+	   }
+	   for(int i=0;i<=n-1;i++)
+	   {
+		   sum*=Math.abs(arr[i]); // absolute Function is Used for Converting the Negative Value into Positive One.(TCS NQT Question)
+	   }
+	   System.out.println("\n Sum of Product of Array:"+sum);
+	}
+}

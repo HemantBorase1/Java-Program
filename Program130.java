974d2b74c34f519191bdd2e5c7507277761cd434
Hemant Borase <hborase004@gmail.com>
2026-04-05 21:23:44 +0530

    Day-12

---
+++ Program130.java
@@ -0,0 +1,27 @@
+class Program130
+{
+   public static void main(String[] args)
+   {
+     int n=5;
+     for(int i=n;i>=1;i--)
+     {
+           for(int j=1;j<=i;j++)
+           {
+            System.out.print(j);
+           }
+           for(int j=i;j<=5;j++)
+           {
+            System.out.print(i);
+           }
+       System.out.println();
+     }
+   }
+}
+/*
+11111
+12222
+12333
+12344
+12345
+
+*/

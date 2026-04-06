aa97af499f737b1381d555e5219b1673047f3e4b
Hemant Borase <hborase004@gmail.com>
2026-04-06 22:30:07 +0530

    Day-13

--- OOP/Demo2.java
+++ OOP/Demo2.java
@@ -1,4 +1,4 @@
-Demo2
+class Demo2
 {
 
     int shiftAtFirst(int n)
@@ -9,8 +9,38 @@ Demo2
          while(temp!=0)
          {
            last*=10;
-           temp/10;
+           temp/=10;
          }
        return last+n;
     }
+
+   int appendTwoDigit(int n1,int n2)
+   {
+            int first=n1;
+            int temp=n2;
+            while(temp!=0)
+            {
+             first*=10;
+             temp/=10;
+            }
+         return first+n2;
+   }
+   
+
+   int reverseNumber(int n)
+   {
+               int rev=0;
+            while(n>0)
+            {
+              int digit=n%10;
+              rev=rev*10+digit;
+              n/=10;
+            }
+          return rev;
+   }
+
+    int checkPalindromNum(int n)
+   {
+
+   }
 }
--- OOP/MainClass.java
+++ OOP/MainClass.java
@@ -3,11 +3,18 @@ class MainClass
 
     public static void main(String[] args)
     {
-           Demo d1=new Demo();
+        /* Demo d1=new Demo();
            int ans=d1.avgOfDigit(23415);
            System.out.println("Average of Number:"+ans);
 
           System.out.println("Find Max Value:"+d1.findMaxDigit(78124));
           System.out.println("Find Min Value:"+d1.findMinDigit(32457));
+       */
+       Demo2 d1=new Demo2();
+       System.out.println("Shift at First:"+d1.shiftAtFirst(57849));
+       
+       System.out.println("\nAppend Two Number at First:"+d1.appendTwoDigit(55,1234));
+       System.out.println("\nReverse of a Number:"+d1.reverseNumber(7849));
+           
     }
 }

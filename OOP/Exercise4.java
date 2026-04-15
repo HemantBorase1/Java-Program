5429abba66ae8fda472d8ac90204fee74866b11b
Hemant Borase <hborase004@gmail.com>
2026-04-15 17:49:42 +0530

    Day-18

--- OOP/Exercise4.java
+++ OOP/Exercise4.java
@@ -4,5 +4,9 @@ class Exercise4
     {
           Demo6 d1=new Demo6();
           System.out.println(d1.factorial(10));
+          System.out.println("------------------------------------------------------------------------");
+          demo7 d2=new demo7();
+          d2.m1();
+          d2.m2(100);
     }
 }
---
+++ OOP/demo7.java
@@ -0,0 +1,12 @@
+class demo7
+{
+    void m1()
+    {
+         System.out.println("m1 Method Called without any Parameter.");
+    }
+
+    void m2(int a)
+    {
+         System.out.println("m2 Method Called with Parameter.");
+    }
+}

6dce3ff4e52625fdc2669e0d4984a4a7665a818a
Hemant Borase <hborase004@gmail.com>
2026-04-18 18:22:55 +0530

    Day-20

---
+++ OOP/Array/Program1.java
@@ -0,0 +1,9 @@
+class Program1
+{
+   public static void main(String[] args)
+   {
+          int a[]=new int[5];
+          for(int i=0;i<a.length;i++)
+              System.out.println(a[i]);
+   }
+}

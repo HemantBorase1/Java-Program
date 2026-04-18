a9be7f597ee60b6a456e9e5f295a88c9e99791f2
Hemant Borase <hborase004@gmail.com>
2026-04-18 18:33:00 +0530

    Day-20

--- OOP/Array/Program1.java
+++ OOP/Array/Program1.java
@@ -5,5 +5,8 @@ class Program1
           int a[]=new int[5];
           for(int i=0;i<a.length;i++)
               System.out.println(a[i]);
+          
+          int b[]={10,20,30,40,50};
+          int c[]=new int [a.length];
    }
 }

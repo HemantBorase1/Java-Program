e12f8b70fe8bc545f50f6e0a1ce7d51aa64ebe6d
Hemant Borase <hborase004@gmail.com>
2026-04-19 17:44:46 +0530

    Day-21

--- OOP/Array/Program2.java
+++ OOP/Array/Program2.java
@@ -8,10 +8,16 @@ class Program2
              System.out.println("Enter Size of the Array:");
              int n=sc.nextInt();
              int arr[]=new int[n];
+             System.out.println("Enter the Array Element:");
              for(int i=0;i<arr.length;i++)
              {
                   arr[i]=sc.nextInt();
              } 
+             System.out.println("Array Elements:");
+             for(int i=0;i<arr.length;i++)
+             {
+                System.out.print(arr[i]+"\t");
+             }
              
       }
 }

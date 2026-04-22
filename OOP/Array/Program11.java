75e43b9a362688d2b66b05efc536c796e7a0bd20
Hemant Borase <hborase004@gmail.com>
2026-04-22 16:02:11 +0530

    Day-23

--- OOP/Array/Program11.java
+++ OOP/Array/Program11.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 class Program11
 {
     public static void main(String[] args)
     {
         int a[]={10,20,30,40,50};
-        for(int i=0;i<a.length/2;i++)
+        for(int i=0;i<a.length/2;i++) // 2<2
         {
              a[i]+=a[a.length-1-i];
              a[a.length-1-i]=a[i]-a[a.length-1-i];
@@ -13,3 +14,15 @@ class Program11
 
     }
 }
+
+/*
+[10,20,30,40,50]
+  0  1  2  3  4
+ 60
+              10
+  50,20,30,40,10
+   0  1  2  3  4
+  50 60    20
+  50 40 30 40 10
+   0  1  2  3  4
+*/

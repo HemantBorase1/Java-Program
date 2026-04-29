6a3af978bf3402e57f3e6408dc421df01f242db9
Hemant Borase <hborase004@gmail.com>
2026-04-29 11:54:25 +0530

    Day-30

---
+++ OOP/Array/Program24.java
@@ -0,0 +1,19 @@
+class Program24
+{
+
+    public static void main(String[] args)
+    {
+          int a[][]=new int[5][5];
+ 
+          for(int i=0;i<a.length;i++)
+          {
+               for(int j=0;j<a[i].length;j++)
+               {
+                  if(i==j||i+j==a.length-1)
+                  a[i][j]=(i+1)*10;
+                  System.out.print(a[i][j]+"\t");
+                }
+        System.out.println();
+        }
+    }
+}

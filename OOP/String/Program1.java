5bb557120454431ac972c885e0d1498227b49fa2
Hemant Borase <hborase004@gmail.com>
2026-04-30 19:21:30 +0530

    Day 31

--- OOP/String/Program1.java
+++ OOP/String/Program1.java
@@ -3,6 +3,13 @@ class Program1
 
     public static void main(String[] args)
     {
-            String str="India";
+            // codePointAt(int index)
+            // Unicode value of char
+ 
+            System.out.println("ABC".codePointAt(0));
+            
+           // codePointBefore(int index)
+           //  Unicode before index
+            System.out.println("ABC".codePointBefore(2));
     }
 }

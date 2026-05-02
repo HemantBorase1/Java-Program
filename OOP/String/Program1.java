f7a856d461ab8d73841a8fd7c42c870f4b5d60a9
Hemant Borase <hborase004@gmail.com>
2026-05-02 19:32:52 +0530

    Day-33

--- OOP/String/Program1.java
+++ OOP/String/Program1.java
@@ -18,7 +18,37 @@ class Program1
 
           // equals(Object)
            System.out.println("a".equals("a"));
+          
+         // compareTo(String)
+         System.out.println("apple".compareTo("apple"));
             
+        /* 
+compareTo() compares characters one by one using their Unicode values.
+
+'a' = 97
+'A' = 65
+
+So the comparison starts with:
+
+'a' - 'A' = 97 - 65 = 32
+
+Since the very first characters are different, Java stops there and returns 32.
+
+Key takeaway:
+Lowercase letters have higher Unicode values than uppercase letters
+So "apple" is considered greater than "APPLE"
+
+        */
+
+     // Prefix / Suffix
+     // Syntax: startsWith(String)
+    System.out.println("Java".startsWith("Ja"));
+
+    // startsWith(String,int offset)
+    System.out.println("Hello".startsWith("ll",2)); 
+
+    // endsWith(String)
+    System.out.println("Java".endsWith("va")); 
           
     }
 }

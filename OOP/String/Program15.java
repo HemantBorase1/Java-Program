b784eba1cd46b010497175beda35cd1ebfb749f6
Hemant Borase <hborase004@gmail.com>
2026-05-08 18:42:34 +0530

    Day-36

--- OOP/String/Program15.java
+++ OOP/String/Program15.java
@@ -2,6 +2,15 @@ class Program15
 {
    public static void main(String[] args)
    {
-       
+       String s1="welcome to international airport";
+       String s2[]=s1.split(" ");
+       String max=s2[0];
+       for(int i=1;i<s2.length;i++)
+       {
+            String temp=s2[i];
+            if(max.length()<=temp.length())
+                  max=temp;
+       }
+      System.out.println(max);
    }
 }

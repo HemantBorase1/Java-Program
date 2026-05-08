49008da68540e9670330eb456b830d8c01e06cc5
Hemant Borase <hborase004@gmail.com>
2026-05-08 19:01:18 +0530

    Day-36

---
+++ OOP/String/Program18.java
@@ -0,0 +1,23 @@
+class Program18
+{
+
+   public static void main(String[] args)
+   {
+       String s1="hello i am java developer";
+       String s2[]=s1.split(" ");
+       StringBuilder sb=new StringBuilder();
+       for(int i=0;i<s2.length;i++)
+       {
+            String temp=s2[i];
+            sb.append(new StringBuilder(temp).reverse());
+            if(i<s2.length-1)
+                sb.append(" ");
+       }
+       System.out.println(sb+".");
+   }
+}
+
+/*
+Input: hello i am java developer
+Output: olleh i ma avaj repoleved.
+*/

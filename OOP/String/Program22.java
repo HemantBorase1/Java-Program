aa2dcc1f95a637898b4cf1da9d1195e83d46c202
Hemant Borase <hborase004@gmail.com>
2026-05-09 07:55:09 +0530

    Day-37

--- OOP/String/Program22.java
+++ OOP/String/Program22.java
@@ -2,6 +2,17 @@ class Program22
 {
    public static void main(String[] args)
    {
-      
+      String s1="Never odd or even";
+      StringBuilder sb=new StringBuilder();
+      for(int i=0;i<s1.length();i++)
+      {
+           char c =s1.charAt(i);
+           if(Character.isLetterOrDigit(c))
+                  sb.append(c);
+      }
+      if(sb.toString().equalsIgnoreCase(sb.reverse().toString()))
+            System.out.println("String is Palindrome.");
+      else
+            System.out.println("String is not Palindrome.");
    }
 }

3e28edbfa9423f0d0bfba741f333b7ea6cd3bd3c
Hemant Borase <hborase004@gmail.com>
2026-05-10 07:07:07 +0530

    Day-38

--- OOP/String/Program26.java
+++ OOP/String/Program26.java
@@ -3,6 +3,15 @@ class Program26
 
    public static void main(String args[])
    {
-     
+     StringBuilder sb=new StringBuilder();
+     int count=10;
+     for(int i=0;i<=2;i++)
+     {
+           if(count==10)
+              sb.append(--count);
+           else
+               sb.append("a");
+     }
+    System.out.println(sb);
    }
 }

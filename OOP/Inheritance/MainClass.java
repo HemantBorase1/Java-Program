b240e4e9ffda3020bc7b376d1c1aa87c7559145a
Hemant Borase <hborase004@gmail.com>
2026-05-13 09:22:33 +0530

    Day-40

--- OOP/Inheritance/MainClass.java
+++ OOP/Inheritance/MainClass.java
@@ -3,7 +3,8 @@ class MainClass
 
    public static void main(String[] args)
    {
-         Demo3 d1=new Demo();
+         Demo2 d1=new Demo3();
          d1.m1();
+         System.out.println(d1.a);
    }
 }

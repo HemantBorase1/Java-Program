c9783f8728ece4cb76dfd0232a2dc8751e80bf02
Hemant Borase <hborase004@gmail.com>
2026-05-14 08:45:59 +0530

    Day-41

--- OOP/String/Program33.java
+++ OOP/String/Program33.java
@@ -2,19 +2,29 @@ class Program33
 {
     public static void main(String[] args)
     {
-          String str="gibblegabbler";
-          char c=' ';
-          for(int i=0;i<str.length();i++)
-          {
-                for(int j=i+1;j<str.length();j++)
+        String str = "gibblegabbler";
+
+        char c = ' ';
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            int count = 0;
+
+            for(int j = 0; j < str.length(); j++)
+            {
+                if(str.charAt(i) == str.charAt(j))
                 {
-                      if(str.charAt(i)!=str.charAt(j))
-                      {
-                            c=str.charAt(i);
-                            break;
-                      }
+                    count++;
                 }
-          }
-        System.out.println("The First Non Repeated Character in String is: "+c);
-     }
+            }
+
+            if(count == 1)
+            {
+                c = str.charAt(i);
+                break;
+            }
+        }
+
+        System.out.println("The First Non Repeated Character in String is: " + c);
+    }
 }

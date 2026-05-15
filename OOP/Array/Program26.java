7f017c16da914f6578807944456cfb66c1bafc67
Hemant Borase <hborase004@gmail.com>
2026-05-15 09:44:55 +0530

    Day-42

--- OOP/Array/Program26.java
+++ OOP/Array/Program26.java
@@ -5,6 +5,8 @@ class Program26
          int a[]={10,20,30,40};
          int min=a[0];
          int max=a[0];
+         int sum=0;
+         int count=0;
          for(int i=0;i<a.length;i++)
          {
               if(max<a[i])
@@ -15,8 +17,16 @@ class Program26
               {
                   min=a[i];
               }
+            if(a[i]!=max&&a[i]!=min)
+            {
+                sum+=a[i];
+                count++;
+            }
             
          }
+       int avg=sum/count;
+         
        System.out.println("Maximum Value :"+max+"\t Minimum value:"+min);
+       System.out.println("Average Element Except Min & Max"+avg);
     }
 }

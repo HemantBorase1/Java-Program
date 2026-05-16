4defbe81b2c9031fc8d3d7d85722e1868b1c3596
Hemant Borase <hborase004@gmail.com>
2026-05-16 08:11:43 +0530

    Day-43

---
+++ OOP/package/MainClass.java
@@ -0,0 +1,15 @@
+import a.b.demo;
+class MainClass
+{
+
+   public static void main(String[] args)
+   {
+    // Accesing by the Qualified Name it can be used to Combine Multiple Methods for in    //   Program 
+     a.b.demo d1=new a.b.demo();
+     d1.a=120;
+     d1.display();
+
+// 
+    
+   }
+}
---
+++ OOP/package/demo.java
@@ -0,0 +1,10 @@
+package a.b;
+public class demo
+{
+
+   public int a;
+   public void display()
+   {
+      System.out.println("a="+a);
+   }
+}

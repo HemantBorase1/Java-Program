4ffd3a0096cae65489a8a5846029b34889cf46c3
Hemant Borase <hborase004@gmail.com>
2026-06-12 08:57:13 +0530

    P-Day-1

--- Practise/Pattern/Pattern1.java
+++ Practise/Pattern/Pattern1.java
@@ -3,7 +3,7 @@ class Pattern1
 
     public static void main(String[] args)
     {
-           for(int i=1;i<=5;i++)
+           for(int i=0;i<=5;i++)
            {
                   for(int j=i;j>=0;j--)
                   {

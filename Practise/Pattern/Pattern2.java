192c354c385458935d2261d6ba73cf74c7dfa2a6
Hemant Borase <hborase004@gmail.com>
2026-06-12 09:01:52 +0530

    P-Day-1

---
+++ Practise/Pattern/Pattern2.java
@@ -0,0 +1,15 @@
+class Pattern2
+{
+
+    public static void main(String[] args)
+    {
+          for(int i=0;i<=5;i++)
+          {
+                for(int j=5;j>=i;i++)
+                {
+                     System.out.println("*");
+                 }
+            System.out.println();
+          }
+    }
+}

9166f7c8941979d538eac040f8230cd2a9603357
Hemant Borase <hborase004@gmail.com>
2026-06-14 08:27:36 +0530

    P-Day-2

--- Practise/Pattern/Pattern3.java
+++ Practise/Pattern/Pattern3.java
@@ -3,13 +3,22 @@ class Pattern3
 
     public static void main(String [] args)
     {
-        for(int i=0;i<=5;i++)
+        for(int i=1;i<=5;i++)
         {
-             for(int j=0;j<=i;j++)
+             for(int j=1;j<=i;j++)
              {
-                   System.out.print(j);
+                   System.out.print(i);
              }
          System.out.println();
         }
     }
 }
+
+/*
+1
+22
+333
+4444
+55555
+
+*/

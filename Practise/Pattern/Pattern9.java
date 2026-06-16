1fb0551339a47ccc5a12774ecaef274a6b64b24c
Hemant Borase <hborase004@gmail.com>
2026-06-16 09:35:40 +0530

    P-Day-3

--- Practise/Pattern/Pattern9.java
+++ Practise/Pattern/Pattern9.java
@@ -3,11 +3,11 @@ class Pattern9
 
   public static void main(String[] args)
   {
-        for(int i=0;i<=5;i++)
+        for(int i=1;i<=5;i++)
         {
-              for(int j=0;j<=i;j++)
+              for(int j=1;j<=i;j++)
               {
-                     System.out.println((char)(j+96));
+                     System.out.print((char)(j+96));
               }
             System.out.println();
         }

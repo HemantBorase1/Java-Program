de259fbb033058eb6aef03adc060e777e002aba4
Hemant Borase <hborase004@gmail.com>
2026-06-19 11:37:45 +0530

    Thread-Update

--- Thread/MainClass.java
+++ Thread/MainClass.java
@@ -14,6 +14,14 @@ class MainClass
 
       t1.start();
       t2.start();
+       try
+       {
+              t1.join(5000);
+              t2.join(5000);
+       }catch(InterruptedException e)
+       {
+            System.out.print(e);
+       }
       t3.start();
 
      /*r.start();

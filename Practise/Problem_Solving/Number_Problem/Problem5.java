3d3e142867f3fa937b69184a8d8da5abac8bdc24
Hemant Borase <hborase004@gmail.com>
2026-06-20 11:33:27 +0530

    P-Day-3

---
+++ Practise/Problem_Solving/Number_Problem/Problem5.java
@@ -0,0 +1,16 @@
+class Problem5
+{
+
+  public static void main(String[] args)
+  {
+       int n=13;
+       if(n%2==0)
+       {
+              System.out.println("Number is Even");
+       }
+       else
+       {
+             System.out.println("Number is Odd");
+       }
+  }
+}

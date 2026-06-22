7dcd9acdef028316465ee4330a6bbf027f10e05d
Hemant Borase <hborase004@gmail.com>
2026-06-22 14:20:49 +0530

    P-Day-5

---
+++ Practise/Problem_Solving/Array_Problem/Problem9.java
@@ -0,0 +1,22 @@
+class Problem9
+{
+
+   public static void main(String [] args)
+   {
+         int arr[]={7,12,20,30};
+         int max=arr[0];
+         int min=arr[0];
+         for(int i=0;i<arr.length;i++)
+         {
+               if(arr[i]>max)
+               {
+                    max=arr[i];
+               }
+               else{
+                 min=arr[i];
+               }
+         }
+        System.out.println(max-min);
+     
+   }
+}

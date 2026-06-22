c38b9d7f3a8f4915d0f17a1f8dd3b1398bdd9fc5
Hemant Borase <hborase004@gmail.com>
2026-06-22 12:03:18 +0530

    Nested Try-Catch Block Example

--- Exception/Program7.java
+++ Exception/Program7.java
@@ -3,13 +3,6 @@ class Program7
 
     public static void main(String[] args)
     {
-             try
-             {
-                 System.out.println(10/0);
-             }
-             catch(ArithmeticException e)
-             {
-                    throw new ArithmeticException("Division by Zero is Not Allowed.");
-             }
+             throw new ArithmeticException("Division by Zero is Not Allowed.");
      }
 }

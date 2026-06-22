259712e8fa5a8e5957b46910c55cbeb4185d71f6
Hemant Borase <hborase004@gmail.com>
2026-06-22 13:41:45 +0530

    P-Day-5

--- Practise/Problem_Solving/String_Problem/Problem10.java
+++ Practise/Problem_Solving/String_Problem/Problem10.java
@@ -4,22 +4,28 @@ class Problem10
    public static void main(String [] args)
    {
          String str="swiss";
-         int count=0;
+         String ch="";
+         
          for(int i=0;i<str.length();i++)
          {
-              char c=str.charAt(i);
-              for(int j=i+1;j<str.length();j++)
-              {
-                   if(c==str.charAt(i))
-                   {
-                      count++;
-                   }
-              }
-           if(count==1)
+           char c=str.charAt(i);    
+           int count=1;
+           for(int j=i+1;j<str.length();j++)
            {
-               System.out.println(c);
+                if(c==str.charAt(j))
+                {
+                     count++;
+                      
+                }
+              
            }
+           if(count==1)
+                      {
+                         ch+=c;
+                         break;
+                      }
          }
+       System.out.println(ch);
          
    }
 }

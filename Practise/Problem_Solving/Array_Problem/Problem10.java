a250c1c82899fccd0eff7a02dc787a18005455bc
Hemant Borase <hborase004@gmail.com>
2026-06-23 13:56:05 +0530

    P-Day-6

--- Practise/Problem_Solving/Array_Problem/Problem10.java
+++ Practise/Problem_Solving/Array_Problem/Problem10.java
@@ -3,9 +3,9 @@ class Problem10
 
   public static void main(String[] args)
   {
-         int arr[]={7,9,10,2};
+         int arr[]={7,9,10,11};
          Boolean b=false;
-         for(int i=0;i<arr.length;i++)
+         for(int i=0;i<arr.length-1;i++)
          {
              if(arr[i]<arr[i+1])
              {
@@ -13,7 +13,15 @@ class Problem10
              }
              else{
                  b=false;
+                 break;
              }       
          }
+       if(b)
+       {
+             System.out.println("Array is Sorted.");
+       }
+       else{
+             System.out.println("Array is Not Sorted.");
+       }
   }
 }

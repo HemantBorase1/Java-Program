a52c99fc7eabdd57806558b6a2397e7e0e3e8b4d
Hemant Borase <hborase004@gmail.com>
2026-06-23 08:46:20 +0530

    Throws Keyword

--- Exception/Program8.java
+++ Exception/Program8.java
@@ -4,6 +4,6 @@ class Program8
    public static void main(String[] args) throws InterruptedException
    {
         Thread.sleep(2000);
-        System.out.println("Executing After 2Sec");
+        System.out.println("Executing After 2 Sec.");
    }
 }

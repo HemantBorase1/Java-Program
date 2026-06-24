c753aa944d9810fd15bc2e46a21f43a3c37728a0
Hemant Borase <hborase004@gmail.com>
2026-06-24 11:36:10 +0530

    Customized Exception

--- Exception/Main.java
+++ Exception/Main.java
@@ -4,7 +4,7 @@ public class Main
    public static void main(String[] args)
    {
            try{
-                Voter.checkAge(16);
+                Voter.checkAge(18);
            }
            catch(InvalidAgeException e)
            {
--- Exception/Voter.java
+++ Exception/Voter.java
@@ -1,15 +1,12 @@
 public class Voter
 {
+    public static void checkAge(int age) throws InvalidAgeException
+    {
+        if(age < 18)
+        {
+            throw new InvalidAgeException("You are not Eligible to Vote.");
+        }
 
-     public static void checkage(int age)
-     {
-           throws InvalidAgeException{
-             
-              if(age<18)
-              {
-                     throw new InvalidAgeException("You are not Eligible to Vote.");
-              }
-           System.out.println("You Are Eligible for Vote");
-         }
-     }
+        System.out.println("You Are Eligible for Vote");
+    }
 }

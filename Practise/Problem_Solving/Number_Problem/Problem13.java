417a7dce1afe93e01c876daa4464deb4cffb2f61
Hemant Borase <hborase004@gmail.com>
2026-06-24 13:35:39 +0530

    P-Day-7

---
+++ Practise/Problem_Solving/Number_Problem/Problem13.java
@@ -0,0 +1,18 @@
+class Problem13
+{
+
+    public static void main(String [] args)
+    {
+        int n=12345;
+        int rev=0;
+ 
+        while(n!=0)
+        {
+             int digit=n%10;
+             rev=(rev*10)+digit;
+             n/=10;
+        }
+
+       System.out.println("Reverse of the Number:"+rev);
+    }
+}

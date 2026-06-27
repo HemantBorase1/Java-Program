fc67f950dac4ed1b973da72782f63de86ae775d7
Hemant Borase <hborase004@gmail.com>
2026-06-27 11:27:33 +0530

    Collectio Day-1

---
+++ Collection/Program1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+class Program1
+{
+
+    public static void main(String[] args)
+    {
+         ArrayList <Object> list=new ArrayList();
+        list.add("Pratik");
+        list.add("Hemant");
+        list.add("Lala");
+        list.add(34);
+        System.out.println(list);
+    }
+}

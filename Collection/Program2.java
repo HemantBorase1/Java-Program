3df4f21893b875a2b5e40852acab80c59f7520ff
Hemant Borase <hborase004@gmail.com>
2026-06-29 08:43:12 +0530

    Program-2

---
+++ Collection/Program2.java
@@ -0,0 +1,14 @@
+class Program2
+{
+
+    public static void main(String[] args)
+    {
+         ArrayList <Object> list=new ArrayList();
+         list.add("Pratik");
+         list.add(10);
+         list.add('A');
+         list.add(5.768);
+  
+         System.out.println(list);
+    }
+}

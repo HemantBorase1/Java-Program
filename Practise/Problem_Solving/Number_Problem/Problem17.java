fe1604597da9f42ac176638611b2411c7ad69064
Hemant Borase <hborase004@gmail.com>
2026-06-29 12:42:01 +0530

    P-Day-9

--- Practise/Problem_Solving/Number_Problem/Problem17.java
+++ Practise/Problem_Solving/Number_Problem/Problem17.java
@@ -1,59 +1,38 @@
 class Problem17
 {
-
+   // Dry-Run this Program
     public static void main(String[] args)
     {
-        int n=69;
-      
-        boolean b=false;
-        int count=0;       
-        double root=Math.sqrt(n);
-        int temp=root;
-        while(temp!=0)
+        int n=625;
+        int square=n*n;
+       
+        int temp=n;
+        int count=0;
+        
+        // it counts digits in number.
+        while(temp>0)
         {
-           int digit=temp%10;
-           count++;
-           n/=10;
-        }
-        while(n!=0)
+             count++;
+             temp/=10;
+        }   
+        // power of 10 based in digit count    
+        int power=1;
+        for(int i=0;i<count;i++)
         {
-         if(count==1)
-         {
-          int digit =n%10;
-          if(digit==temp)
-          {
-               b=true;
-               break;
-          }
-          else
-          {
-               b=false;
-          }
-         }
-         if(count==2)
-         {
-            int digit =n%100;
-          if(digit==temp)
-          {
-               b=true;
-               break;
-          }
-          else
-          {
-               b=false;
-          }
-         }
-         n/=10;
-         
+              power*=10;
         }
+        // extract last digits of square
 
-        if(b)
+        int lastDigits = square % power;
+  
+        // check condition
+        if(lastDigits==n)
         {
-            System.out.println("Automorphic Number");
+            System.out.println("Antomorphic Number");
         }
         else
         {
-            System.out.println("Not Automorphic Number");
+             System.out.println("Not Automorphic Number");
         }
     }
 }

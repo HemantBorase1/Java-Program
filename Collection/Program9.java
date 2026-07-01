c9f8675dc8114105794eda0a251c77ac88c2aeaa
Hemant Borase <hborase004@gmail.com>
2026-07-01 09:07:53 +0530

    LinkedHashSet

---
+++ Collection/Program9.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashSet;
+class Program9
+{
+
+     public static void main(String[] args)
+     {
+            LinkedHashSet ls=new LinkedHashSet();
+            ls.add(50);
+            ls.add(17);
+            ls.add(51);
+            ls.add(10);
+            ls.add("Ketan");
+            ls.add("Chetan");
+            System.out.println(ls);
+     }
+}

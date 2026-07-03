d7cf821780cebb3198dd91c1de3fdebd02747e50
Hemant Borase <hborase004@gmail.com>
2026-07-03 13:19:37 +0530

    Collection Program

--- Collection/Program13.java
+++ Collection/Program13.java
@@ -1,16 +1,17 @@
 import java.util.concurrent.ArrayBlockingQueue;
-class Program13
-{
 
-    public static void main(String[] args)
-    {
-            throws InterruptedException{
-                ArrayBlockingQueue<Integer> queue=new ArrayBlockingQueue<>();
-           
-               queue.put(10);
-               queue.put(10);
-               queue.put(30);
-               System.out.println(queue);
-            }
+class Program13 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(5);
+
+        queue.put(10);
+        queue.put(20);
+        queue.put(30);
+
+        System.out.println(queue);
+        queue.peek();
+        System.out.println(queue);
     }
 }

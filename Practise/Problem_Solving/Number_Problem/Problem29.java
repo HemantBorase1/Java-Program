aafe26e3b6b609868bdea303b372039f110e13f2
Hemant Borase <hborase004@gmail.com>
2026-07-11 13:27:23 +0530

    P-Day-14

--- Practise/Problem_Solving/Number_Problem/Problem29.java
+++ Practise/Problem_Solving/Number_Problem/Problem29.java
@@ -1,40 +1,42 @@
-class Problem29
-{
+class Problem29 {
 
-   public static void main(String[] args){
-   
-    int n=2025;
-    int n2=n;
-    int sum=0;
-    int temp=n;
-    while(n!=0){
-         count++;
-         n/=10;
+    public static void main(String[] args) {
 
-     }
-    if(count==2){
-         while(n2!=0)
-         {
-             int digit=n2%10;
-             sum+=digit;
-             n/=10;
-         }
-     }else if(count==4){
-          while(n2!=0)
-         {
-             int digit=n2%100
-             sum+=digit;
-             n/=100;
-         }
+        int n = 2026;
+        int temp = n;
+
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            n /= 10;
         }
-       else{
-        System.out.println("This is Not Tech Number");
-       }
-      if(temp==sum){
-          System.out.println("Tech Number");
-      }
-      else{
-        System.out.println("Tech Number");
+
+        int sum = 0;
+
+        if (count == 2) {
+
+            int first = temp / 10;
+            int second = temp % 10;
+
+            sum = first + second;
+
+        } else if (count == 4) {
+
+            int first = temp / 100;
+            int second = temp % 100;
+
+            sum = first + second;
+
+        } else {
+            System.out.println("This is Not Tech Number");
+            return;
         }
-   }
+
+        if (sum * sum == temp) {
+            System.out.println("Tech Number");
+        } else {
+            System.out.println("This is Not Tech Number");
+        }
+    }
 }

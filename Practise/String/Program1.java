b281438e8a6c95689cefb0e574b12c7ebba31b18
Hemant Borase <hborase004@gmail.com>
2026-07-11 20:57:34 +0530

    String -Concept

--- Practise/String/Program1.java
+++ Practise/String/Program1.java
@@ -2,12 +2,19 @@ class Program1{
 
       public static void main(String[] args){
            String s1=new String("Durga");
+           String s2=new String("Durga");
            s1.concat("Solution");
            s1=s1.concat("Software");
            System.out.println(s1);
+           System.out.println(s1==s2);
+           System.out.println(s1.equals(s2));
       }
 }
 
-// Output:
+/* Output:
 
 DurgaSoftware
+
+
+
+*/

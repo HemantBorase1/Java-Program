6d0083c4883fb8b168232b5f215d4f11dd95e1dd
Hemant Borase <hborase004@gmail.com>
2026-07-12 15:39:13 +0530

    OOPs-Started

---
+++ OOP/Class/MainClass.java
@@ -0,0 +1,13 @@
+class MainClass
+{
+
+     public static void main(String[] args)
+     {
+            Student s1=new Student();
+            s1.id=101;
+            s1.name="Hemant";
+            s1.address="Jalgoan";
+            s1.CGPA=8.27;
+            s1.display();
+     }
+}
---
+++ OOP/Class/Student.java
@@ -0,0 +1,13 @@
+class Student
+{
+     public int id;
+     public String name;
+     public String address;
+     public double CGPA;
+
+     void display()
+     {
+         System.out.println(id+"\t"+name+"\t"+address+"\t"+CGPA);
+     }
+ 
+}

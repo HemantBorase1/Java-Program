9c98b4bcd74d062fb52f5b9d682a7e1d5cdbfb73
Hemant Borase <hborase004@gmail.com>
2026-07-12 16:20:58 +0530

    OOPs

--- OOP/Class/MainClass.java
+++ OOP/Class/MainClass.java
@@ -3,11 +3,13 @@ class MainClass
 
      public static void main(String[] args)
      {
-            Student s1=new Student();
-            s1.id=101;
+            Student s1=new Student(102,"Hemant","Jalgoan",8.27);
+         /*   s1.id=101;
             s1.name="Hemant";
             s1.address="Jalgoan";
             s1.CGPA=8.27;
+           
+         */
             s1.display();
      }
 }
--- OOP/Class/Student.java
+++ OOP/Class/Student.java
@@ -5,6 +5,25 @@ class Student
      public String address;
      public double CGPA;
 
+     Student()
+     {
+          id=1;
+          name="OM";
+          address="Pune";
+          CGPA=10;
+        //  display();
+     }
+     Student(int id,String name){
+         this.id=id;
+         this.name=name;
+     }
+     Student(int id,String name,String address,double CGPA)
+     {
+         this.id=id;
+         this.name=name;
+         this.address=address;
+         this.CGPA=CGPA;
+     }
      void display()
      {
          System.out.println(id+"\t"+name+"\t"+address+"\t"+CGPA);

8122c13344d729291fbf47740b7081d447cda9d5
Hemant Borase <hborase004@gmail.com>
2026-07-14 13:41:40 +0530

    P-Day-1

--- Practise/Problem_Solving/String_Problem/Problem1.java
+++ Practise/Problem_Solving/String_Problem/Problem1.java
@@ -1,20 +1,19 @@
 class Problem1
 {
-       public static void main(String[] args)
-       {
-              String str="Education";
-              int count=0;
-              String vowels="AEIOUaeiou";
-              for(int i=0;i<str.length();i++)
-              {
-                    for(int j=0;j<vowels.length();j++)
-                    {
-                         if(str.charAt(i)==vowels.charAt(j))
-                         {
-                             count++;
-                         }
-                    }
-              }
-             System.out.println("Total Vowels in the String:"+count);
-       }
+
+      public static void main(String[] args)
+      {
+           String str="Programming";
+           String lowercase=str.toLowerCase();
+           int count=0;
+           for(int i=0;i<str.length();i++)
+           {
+                char c=lowercase.charAt(i);
+                if(c!='a'&& c!='e'&& c!='i' && c!='o' && c!='u')
+                {
+                   count++;
+                 }
+           }
+           System.out.println(count);
+      }
 }

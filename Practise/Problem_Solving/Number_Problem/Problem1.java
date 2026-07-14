2130396b9b1bf07b72d72fd2d5375f5d49347f25
Hemant Borase <hborase004@gmail.com>
2026-07-14 13:22:21 +0530

    P-Day-1

--- Practise/Problem_Solving/Number_Problem/Problem1.java
+++ Practise/Problem_Solving/Number_Problem/Problem1.java
@@ -1,16 +1,14 @@
 class Problem1
 {
 
-    public static void main(String[] args)
-    {
-          int n=12564763;
-          int count=0;
-          while(n!=0)
-          {
-             
-             n=n/10;
-             count++;
-          }
-       System.out.println("Total Digit in Number:"+count);
-     }
+    public static void main(String args[]){
+         int n=5824;
+         int sum=0;
+         while(n!=0){
+             int digit=n%10;
+             sum+=digit;
+             n/=10;
+        }
+      System.out.println(sum);
+    }
 }

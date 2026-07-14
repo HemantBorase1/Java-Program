5350b82b958716a7647a346d19a501dbf99e206e
Hemant Borase <hborase004@gmail.com>
2026-07-14 13:58:12 +0530

    P-Day-1

--- Practise/Problem_Solving/Array_Problem/Problem1.java
+++ Practise/Problem_Solving/Array_Problem/Problem1.java
@@ -1,16 +1,16 @@
 class Problem1
 {
+
     public static void main(String[] args)
     {
-         int arr[]={4,11,6,9};
-         int max=arr[0];
+         int arr[]={14, 8, 25, 6, 17};
+         int min=arr[0];
          for(int i=0;i<arr.length;i++)
          {
-             if(arr[i]>max)
-             {
-                max=arr[i];
+             if(arr[i]<min){
+               min=arr[i];
              }
          }
-        System.out.println("Largest Element in the Array:"+max);
+        System.out.println(min);
     }
 }

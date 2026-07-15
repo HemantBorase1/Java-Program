862945ff2a3f5f2d90e538f5c45aa24b9551f7fb
Hemant Borase <hborase004@gmail.com>
2026-07-15 21:39:53 +0530

    P-Day-2

---
+++ Practise/Problem_Solving/Number_Problem/Problem6.java
@@ -0,0 +1,18 @@
+class Problem6
+{
+
+     public static void main(String[] args)
+     {
+            int n=965214;
+            int min=2;
+            while(n!=0)
+            {
+                  int digit=n%10;
+                  if(digit<min)
+                  {
+                      min=digit;
+                  }
+            }
+           System.out.println(min);
+     }
+}

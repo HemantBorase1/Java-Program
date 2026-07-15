aa43874618476f4f911bc6009a7250d4d8725456
Hemant Borase <hborase004@gmail.com>
2026-07-15 21:45:32 +0530

    P-Day-2

---
+++ Practise/Problem_Solving/Array_Problem/Problem4.java
@@ -0,0 +1,19 @@
+class Problem4
+{
+
+     public static void main(String[] args)
+     {
+           String str="programming";
+           
+           int count=0;
+           for(int i=0;i<str.length();i++)
+           {
+               char c=str.charAt(i);
+               if(c=='g')
+               {
+                     count++;
+               }
+           }
+           System.out.println(count++);
+     }
+}

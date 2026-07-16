8de5718afa4bb8915d256a5a30976434327b0d40
Hemant Borase <hborase004@gmail.com>
2026-07-16 11:41:37 +0530

    P-Day-3

--- Practise/Problem_Solving/Array_Problem/Problem4.java
+++ Practise/Problem_Solving/Array_Problem/Problem4.java
@@ -1,19 +1,18 @@
 class Problem4
 {
 
-     public static void main(String[] args)
-     {
-           String str="programming";
-           
-           int count=0;
-           for(int i=0;i<str.length();i++)
-           {
-               char c=str.charAt(i);
-               if(c=='g')
-               {
-                     count++;
-               }
-           }
-           System.out.println(count++);
-     }
+    public static void main(String[] args)
+    {
+        int arr[]={12, 25, 40, 18, 25};
+        int Target=18;
+        int index=0;
+        for(int i=0;i<arr.length;i++)
+        {
+          if(arr[i]==Target)
+          {
+               index+=i;
+          }
+        }
+       System.out.println(index);
+    }
 }

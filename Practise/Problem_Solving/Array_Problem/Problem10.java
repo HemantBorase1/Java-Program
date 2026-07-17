3ea164bc36ed71a3d6a077dd1a862350e8c34632
Hemant Borase <hborase004@gmail.com>
2026-07-17 11:46:53 +0530

    P-Day-4

---
+++ Practise/Problem_Solving/Array_Problem/Problem10.java
@@ -0,0 +1,14 @@
+class Problem10
+{
+
+    public static void main(String[] args)
+    {
+         int arr[]={0, 20, 30, 40};
+         int sum=0;
+         for(int i=0;i<arr.length;i++)
+         {
+               sum+=arr[i];
+         } 
+         System.out.println(sum/arr.length);
+    }
+}

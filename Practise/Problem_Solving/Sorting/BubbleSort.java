c3c96b1cf1aa16758b8e7df37f0c60eff54280c0
Hemant Borase <hborase004@gmail.com>
2026-07-20 09:11:24 +0530

    P-Day-8

---
+++ Practise/Problem_Solving/Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class BubbleSort
+{
+ 
+      public static void main(String[] args)
+      {
+            int arr[]={7,2,3,0,4};
+            for(int i=0;i<arr.length;i++)
+            {
+                  for(int j=0;j<arr.length-1;j++)
+                  {
+                        if(arr[j]>arr[j+1])
+                        {
+                            int temp=arr[j];
+                            arr[j]=arr[j+1];
+                            arr[j+1]=temp;           
+                        }
+                  }
+            }
+            System.out.println("Sorted Using Bubble Sort:"+Arrays.toString(arr));
+      }
+}

bff829f8c154c26bb6a242fcefbac33e5da7e076
Hemant Borase <hborase004@gmail.com>
2026-07-27 22:30:20 +0530

    P-Day-12

--- Practise/Problem_Solving/Array_Problem/Problem20.java
+++ Practise/Problem_Solving/Array_Problem/Problem20.java
@@ -7,16 +7,32 @@ class Problem20
           int arr1[]={1,4,7};
           int arr2[]={2,3,8};
           int merge[]=new int[arr1.length+arr2.length];
-          for(int i=0;i<merge.length;i++)
-          {
-               int j=0;
-               while(arr1[j]<arr2[j])
+         int i=0,j=0,k=0;
+     
+         while(i<arr1.length && j<arr2.length)
+         {
+              if(arr1[i]<arr2[j])
+              {
+                   merge[k]=arr1[i];
+                   i++;
+               } else
                {
-                  merge[i]=arr1[j];
+                    merge[k]=arr2[j];
+                    j++;
                }
-             merge[i]=arr2[j];
+             k++;
+         }
+         while(i<arr1.length){
+              merge[k]=arr1[i];
+              i++;
+              k++;
+         }
+         while(j<arr2.length)
+         {
+             merge[k]=arr2[j];
              j++;
-          }
-        System.out.println(Arrays.toString(merge));
+             k++;
+         }
+       System.out.println(Arrays.toString(merge));
      }
 }

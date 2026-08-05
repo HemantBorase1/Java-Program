30149e2b85a63c420bf71fb1aa7b963629858578
Hemant Borase <hborase004@gmail.com>
2026-08-05 12:32:58 +0530

    P-Day-18

--- Practise/Problem_Solving/Array_Problem/Problem27.java
+++ Practise/Problem_Solving/Array_Problem/Problem27.java
@@ -1,31 +1,33 @@
 import java.util.Arrays;
-class Problem27
-{
 
-     public static void main(String[] args)
-     {
-           int arr[]={4, 2, 4, 7, 2, 9, 7};
-           int newarr[]=new int[arr.length];
-            
-           int size=0;
-           for(int i=0;i<arr.length;i++)
-           {
-                isDuplicate=false;
-                for(int j=i+1;j<arr.length;j++)
-                {
-                     if(arr[i]==arr[j])
-                     {
-                        isDuplicate=true;
-                        break;
-                     }
+class Problem27 {
+    public static void main(String[] args) {
+        int arr[] = {4, 2, 4, 7, 2, 9, 7};
+        int newarr[] = new int[arr.length];
+
+        int size = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean isDuplicate = false;
+
+            // Check if element already exists in newarr
+            for (int j = 0; j < size; j++) {
+                if (arr[i] == newarr[j]) {
+                    isDuplicate = true;
+                    break;
                 }
-              if(isDuplicate)
-              {
-                  newarr[size]=arr[i];
-                  size++;
-              }
-           }
-              
-          System.out.println(Arrays.toString(newarr));
-     }
+            }
+
+            // Add only first occurrence
+            if (!isDuplicate) {
+                newarr[size] = arr[i];
+                size++;
+            }
+        }
+
+        // Print only filled elements
+        for (int i = 0; i < size; i++) {
+            System.out.print(newarr[i] + " ");
+        }
+    }
 }

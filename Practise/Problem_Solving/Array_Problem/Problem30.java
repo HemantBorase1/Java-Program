2d7c3057391add064297b2eab369dce0c2d5a0d8
Hemant Borase <hborase004@gmail.com>
2026-08-07 10:40:33 +0530

    P-Day-20

--- Practise/Problem_Solving/Array_Problem/Problem30.java
+++ Practise/Problem_Solving/Array_Problem/Problem30.java
@@ -5,6 +5,8 @@ class Problem30
       {
             int arr[]={12,45,8,27,39};
             int max=0;
+            int found1=0,found2=0;
+            
             for(int i=0;i<arr.length;i++)
             {
                    for(int j=i+1;j<arr.length;j++)
@@ -12,9 +14,12 @@ class Problem30
                         if(arr[i]+arr[j]>max)
                         {
                              max=arr[i]+arr[j];
+                             found1=i;
+                             found2=j;
                         }
                    }
             }
           System.out.println(max);
+          System.out.println("Pair:"+"("+arr[found1]+","+arr[found2]+")");
       }    
 }
